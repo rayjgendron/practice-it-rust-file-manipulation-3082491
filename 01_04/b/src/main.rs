f0946e02b20fdf5ff19e3ddbb1fd8d6bb96def7f
rayjgendron <137917340+rayjgendron@users.noreply.github.com>
2024-07-04 15:53:10 +0000

    wrote expected to code to get minimal function desired...

--- 01_04/b/src/main.rs
+++ 01_04/b/src/main.rs
@@ -1,3 +1,10 @@
 use std::fs::File;
+use std::io::{self, BufRead};
 
-fn main() {}
+fn main() {
+    let file: File = File::open("file_with_lines").unwrap();
+    let reader = io::BufReader::new(file);
+    for line in reader.lines(){
+        println!("{}", line.unwrap());
+    }
+}
