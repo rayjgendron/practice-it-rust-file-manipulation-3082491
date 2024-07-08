73bfdcc2814e3658a6792e50f04e6006c8df5d22
rayjgendron <137917340+rayjgendron@users.noreply.github.com>
2024-07-08 19:53:12 +0000

    did the deed...

--- 02_07/b/src/main.rs
+++ 02_07/b/src/main.rs
@@ -1,7 +1,12 @@
+use std::fs;
+
 fn main() {
-    let file_path = "test_file";
+    let file_path = "no_file";
     
-    // Implement the read_file function
-    //let contents = read_file(&file_path).expect(&format!("unable to read file <{}>", file_path));
-    //println!("{}", contents);
+    let contents = read_file(&file_path).expect(&format!("unable to read file <{}>", file_path));
+    println!("{}", contents);
 }
+
+fn read_file(path: &str) -> Result<String, std::io::Error> {
+    fs::read_to_string(path) 
+ }
