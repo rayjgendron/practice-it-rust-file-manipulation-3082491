9ea26d1f814f6edbe10b4abfec27dcaa2bfb65ad
rayjgendron <137917340+rayjgendron@users.noreply.github.com>
2024-07-11 20:09:29 +0000

    did the deed

--- 04_02/b/src/main.rs
+++ 04_02/b/src/main.rs
@@ -1,9 +1,25 @@
+use std::collections::HashMap;
+
 fn main() {
     let contents = String::from("This is the first line\nThe second line is a little longer\nLine 3 is short\nThe 4th line is the first non-prime\nThe 5th line has the starting five");
 
     // Implement the get_words and word_cout functions
-    //let words = get_words(&contents);
-    //let counter = word_count(&words);
+    let words = get_words(&contents);
+    let counter = word_count(&words);
 
-    //println!("{:#?}", counter);
+    println!("{:#?}", counter);
+}
+
+fn get_words(text: &str) -> Vec<String> {
+    text.split_whitespace()
+        .map(|word| word.to_string())
+        .collect()
+}
+
+fn word_count(words: &Vec<String>) -> HashMap<String, u32> {
+    let mut counts: HashMap<String, u32> = HashMap::new();
+    for word in words {
+        counts.entry(word.to_lowercase()).and_modify(|count| *count += 1).or_insert(1);
+    }
+    counts
 }
